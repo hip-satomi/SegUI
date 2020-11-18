de957800a6ffcf310c62043c47555982c05aa808
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2020-11-18 12:08:14 +0100

    Fixed component import error

--- src/app/shared-components-module/shared-components.module.ts
+++ src/app/shared-components-module/shared-components.module.ts
@@ -1,7 +1,6 @@
 import { ImageDisplayComponent } from './../components/image-display/image-display.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PopoverCompComponent } from '../components/popover-comp/popover-comp.component';
 import { SegmentationComponent } from '../components/segmentation/segmentation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
@@ -9,8 +8,8 @@ import { IonicModule } from '@ionic/angular';
 
 
 @NgModule({
-  declarations: [ImageDisplayComponent, PopoverCompComponent, SegmentationComponent],
-  exports: [ImageDisplayComponent, PopoverCompComponent, SegmentationComponent],
+  declarations: [ImageDisplayComponent, SegmentationComponent],
+  exports: [ImageDisplayComponent, SegmentationComponent],
   imports: [
     CommonModule,
     FormsModule,
