fff85e43c028f599a2722915f92f2ce54df3448c
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2021-12-15 15:18:05 +0100

    Update http interceptor
    - fix omero auth usage

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -1,18 +1,18 @@
-import { catchError, delayWhen, retryWhen, shareReplay, switchMap, tap, map, concatMap, mergeMap, take, delay } from 'rxjs/operators';
-import { AuthService, NoValidTokenException } from './auth.service';
+import { catchError, tap} from 'rxjs/operators';
+import { AuthService } from './auth.service';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, merge, timer, from, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { ToastController } from '@ionic/angular';
+import { NavigationExtras, Router } from '@angular/router';
 import { OmeroAuthService } from './omero-auth.service';
-import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor(private authService: AuthService,
+  constructor(private omeroAuthService: OmeroAuthService,
               private toastController: ToastController,
               private router: Router) { }
 
@@ -22,15 +22,15 @@ export class TokenInterceptorService implements HttpInterceptor {
     .pipe(
       tap(() => console.log(this.router.url)),
       catchError(err => {
-        if ([401, 403].includes(err.status) && this.authService.user) {
-            // auto logout if 401 or 403 response returned from api
-            this.authService.logout(this.router.url);
-            this.showAuthNotValid();
+        if ([401, 403].includes(err.status) && this.omeroAuthService.user) {
+          // auto logout if 401 or 403 response returned from api
+          this.relogin();
+          this.showAuthNotValid();
         }
 
         if(err.status == 404 && err.url.includes('webclient/login/')) {
           // authentication service redirects because of expired token
-          this.authService.logout();
+          this.relogin();
           this.showAuthNotValid();
         }
 
@@ -45,5 +45,17 @@ export class TokenInterceptorService implements HttpInterceptor {
       message: 'Your authentication has expired! Please login again!',
       duration: 5000
     }).then(toast => toast.present());
+  }
+
+  /**
+   * Just navigate to the login page with old url as redirect param
+   */
+  relogin() {
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+        r: this.router.url
+      }
+    };
+    this.router.navigateByUrl('/', navigationExtras);
   }
 }
