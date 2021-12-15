d2dfe8c63ec50e8b319c0f5db91eae2c01b8263e
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2021-12-15 16:06:17 +0100

    Make users aware of connectivity issues
    - this important to make them aware that the app has problems
    - this is to prevent data loss

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -14,18 +14,27 @@ import { UserQuestionsService } from './user-questions.service';
 export class TokenInterceptorService implements HttpInterceptor {
 
   relogin$ = new Subject();
+  unknownConErrors$ = new Subject();
 
   constructor(private omeroAuthService: OmeroAuthService,
               private userQuestionService: UserQuestionsService,
               private router: Router) {
 
-                // listen for relogin events and call the specific function
-                this.relogin$.pipe(
-                  // throttle for 5s
-                  throttleTime(5000),
-                  tap(() => this.__relogin())
-                ).subscribe();
-              }
+    // listen for relogin events and call the specific function
+    this.relogin$.pipe(
+      // throttle for 5s
+      throttleTime(5000),
+      tap(() => this.__relogin())
+    ).subscribe();
+
+    this.unknownConErrors$.pipe(
+      // throttle for 5s
+      throttleTime(5000),
+      tap(() => {
+        this.userQuestionService.showError("We are experience connection problems! Please check your internet connection or contact your admin!", 5000);
+      })
+    ).subscribe();
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -37,12 +46,13 @@ export class TokenInterceptorService implements HttpInterceptor {
           // auto logout if 401 or 403 response returned from api
           this.relogin$.next();
           this.showAuthNotValid();
-        }
-
-        if(err.status == 404 && err.url.includes('webclient/login/')) {
+        } else if(err.status == 404 && err.url.includes('webclient/login/')) {
           // authentication service redirects because of expired token
           this.relogin$.next();
           this.showAuthNotValid();
+        } else {
+          // unknown connectivity issues
+          this.unknownConErrors$.next(err);
         }
 
         const error = (err && err.error && err.error.message) || err.statusText;
