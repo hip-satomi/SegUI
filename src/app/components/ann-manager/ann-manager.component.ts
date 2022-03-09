c8cc163ca98031ff58462ac071c1824ac36a2980
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2022-03-09 10:53:20 +0100

    Update annotation manager
    - add documentation
    - fix code

--- src/app/components/ann-manager/ann-manager.component.ts
+++ src/app/components/ann-manager/ann-manager.component.ts
@@ -12,25 +12,30 @@ import { UserQuestionsService } from 'src/app/services/user-questions.service';
 })
 export class AnnManagerComponent implements OnInit {
 
-  //@Input() labels: AnnotationLabel[];
+  /** Reference to global segmentation model */
   @Input() globalSegModel: GlobalSegmentationModel;
-
-  @Output() redraw = new EventEmitter<void>();
 
   constructor(private userQuestions: UserQuestionsService) { }
 
   ngOnInit() {}
 
+  /**
+   * Add a new label
+   */
   addLabel() {
+
+    // start with cell label
     const names = this.labels.map(l => l.name);
     let newName: string = 'Cell';
 
+    // choose appropriate number if label name already exists
     if (names.includes(newName)) {
         for (let i = 1; names.includes(newName); i += 1) {
             newName = `Cell ${i}`;
         }
     }
 
+    // add label to the seg model
     this.globalSegModel.addAction(new AddLabelAction(new AnnotationLabel(this.globalSegModel.nextLabelId(), newName, true, 'random', true)));
   }
 
@@ -44,8 +49,6 @@ export class AnnManagerComponent implements OnInit {
 
   changeVisibility(label: AnnotationLabel, visible: boolean) {
     this.globalSegModel.addAction(new ChangeLabelVisibilityAction(label.id, visible));
-
-    this.redraw.emit();
   }
 
   get labels(): Array<AnnotationLabel> {
@@ -53,10 +56,12 @@ export class AnnManagerComponent implements OnInit {
   }
 
   changeActivity(label: AnnotationLabel, active: boolean) {
-    if (label.active) {
+    if (label.active == active) {
+      // if we already have the correct activity state skip
       return;
     }
 
+    // set active label
     this.globalSegModel.addAction(new ChangeLabelActivityAction(label.id, active));
   }
 
@@ -64,6 +69,7 @@ export class AnnManagerComponent implements OnInit {
     // does the new name already exist?
     const candidates = this.globalSegModel.labels.filter(l => l.name == newName);
     if (candidates.length > 0) {
+        // ask the user whether he wants to merge labels
         this.userQuestions.mergeLabels(label.name, candidates[0].name).pipe(
             tap((result: boolean) => {
                 if(result) {
@@ -72,6 +78,7 @@ export class AnnManagerComponent implements OnInit {
             })
         ).subscribe();
     } else {
+        // just change the name
         this.globalSegModel.addAction(new RenameLabelAction(label.id, newName));
     }
   }
