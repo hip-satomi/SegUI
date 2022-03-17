cc4d829cc27d8896fb1b9676d51eb740fe1cd97f
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2022-03-17 11:42:07 +0100

    Fix storage service test

--- src/app/services/storage.service.spec.ts
+++ src/app/services/storage.service.spec.ts
@@ -1,12 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { StorageService } from './storage.service';
+
+class MockStorage {
+
+  data = {};
+
+  create() {
+    return this;
+  }
+
+  async set(key: string, value: string) {
+    this.data[key] = value;
+  }
+
+  async get(key: string) {
+    this.data[key] || null;
+  }
+
+  async keys() {
+    return Object.keys(this.data);
+  }
+
+
+}
 
 describe('StorageService', () => {
   let service: StorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [IonicStorageModule.forRoot()],
+      providers: [{
+        provide: Storage,
+        useClass: MockStorage
+      }]
+    });
     service = TestBed.inject(StorageService);
   });
 
