19c195a5c605ec73fe774aa6fe6e8026a81fb296
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2022-07-05 09:35:31 +0200

    Add test includes

--- src/app/components/manual-tracking/manual-tracking.component.spec.ts
+++ src/app/components/manual-tracking/manual-tracking.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { IonicModule } from '@ionic/angular';
 
 import { ManualTrackingComponent } from './manual-tracking.component';
