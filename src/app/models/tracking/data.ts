473ab37cfc26799981f647e9cf0aede47c6152b4
Johannes Seiffarth <j.seiffarth@fz-juelich.de>
2022-09-12 14:47:30 +0200

    Increase tracking data efficiency
    - fast access for incoming/outgoing links

--- src/app/models/tracking/data.ts
+++ src/app/models/tracking/data.ts
@@ -21,6 +21,9 @@ export class TrackingData implements ClearableStorage {
     // array of linkings (all information for the tracking)
     links: Array<Link> = [];
 
+    outgoingLinks = new Map<string, Array<Link>>();
+    incomingLinks = new Map<string, Array<Link>>();
+
     // array of forced track ends containing id of final items
     forcedTrackEnds: Set<string> = new Set<string>();
 
@@ -52,6 +55,11 @@ export class TrackingData implements ClearableStorage {
         // do we already have the same link?
         if(!this.hasLink(link)) {
             this.links.push(link);
+
+            // add outgoing and incoming links
+            this.outgoingLinks[link.sourceId] = [...(this.outgoingLinks.get(link.sourceId) || []), link];
+            this.incomingLinks[link.targetId] = [...(this.incomingLinks.get(link.targetId) || []), link]
+
         }
     }
 
@@ -64,6 +72,11 @@ export class TrackingData implements ClearableStorage {
         const links = this.links.filter(l => l.sourceId == link.sourceId && l.targetId == link.targetId);
         // delete all the candidates
         for (const link of links) {
+
+            // remove outgoing and incoming links
+            this.outgoingLinks[link.sourceId] = this.outgoingLinks[link.sourceId].filter((link: Link) => link.targetId != link.targetId);
+            this.incomingLinks[link.targetId] = this.incomingLinks[link.targetId].filter((link: Link) => link.sourceId != link.sourceId);
+
             const index = this.links.indexOf(link);
             this.links.splice(index, 1);
         }
@@ -75,7 +88,7 @@ export class TrackingData implements ClearableStorage {
      * @returns list of links targeting the segmentation object
      */
     listTo(targetId: string): Array<Link> {
-        return this.links.filter(l => l.targetId == targetId);
+        return (this.incomingLinks[targetId] || []);
     }
 
     /**
@@ -84,6 +97,6 @@ export class TrackingData implements ClearableStorage {
      * @returns list of links originating from the segmentation object
      */
     listFrom(sourceId: string): Array<Link> {
-        return this.links.filter(l => l.sourceId == sourceId);
+        return (this.outgoingLinks[sourceId] || []);
     }
 }
